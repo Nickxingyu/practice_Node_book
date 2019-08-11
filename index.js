0ee4cbf460d53f43398cc7bb15cf96d777502c65
Xingyu <nick19960723@gmail.com>
2019-08-11 17:24:57 +0800

    Add what I do and why I do.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var server=require("./practice");
 var router=require("./router");
 var requestHandler=require("./requestHandler");
 
-var handle={};
+var handle={};//一個類似字典的集合，存放著不同請求路由所對應的請求處理程序
 handle["/"]=requestHandler.start;
 handle["/start"]=requestHandler.start;
 handle["/upload"]=requestHandler.upload;
--- requestHandler.js
+++ requestHandler.js
@@ -1,5 +1,5 @@
-var exec=require("child_process").exec;
-var querystring=require('querystring');
+var exec=require("child_process").exec;//生出小孩去執行，等小孩執行完再回來叫伺服器
+var querystring=require('querystring');//因為取得postData時只需要文字部分，所以用這個分析傳回的text
 function start(response,postData){
     console.log("This is start.");
     var body='<html>'+
@@ -14,17 +14,18 @@ function start(response,postData){
                         '<input type="submit" value="Submit text" />'+
                     '</form>'+
                 '</body>'+
-            '</html>'
+            '</html>'//html文檔，建置一個文字提交框，供使用者提交文字。
+
     exec("ls",{timeout:10000,maxBuffer:20000*1024},function (error, stdout, stderr) {
         response.writeHead(200, {"Content-Type": "text/html"});
         response.write(body);
         response.end();
-      });
+      });//讓child去執行就可以實現non-blocking.
 };
 function upload(response,postData){
     console.log("This is upload.");
     response.writeHead(200,{"Content-type":"text/plain"});
-    response.write("You sent "+querystring.parse(postData).text+'.');
+    response.write("You sent "+querystring.parse(postData).text+'.');//使用querystring取得text的部分
     response.end();
 };
 
